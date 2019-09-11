1aaa53a69f9dd40ab67825c88009cd413dc77db5
cansolve <tongxiang608@gmail.com>
2019-09-11 13:48:25 +0800

    new add

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Layout from '@icedesign/layout';
 
-import ReviewDetailInfo from './components/ReviewDetailInfo';
 import ReviewOverview from './components/ReviewOverview';
 import ReviewRequestTable from './components/ReviewRequestTable';
+import Aside from './components/Aside';
+import InsideMainRoutes from './InsideMainRoutes';
 
 export default class Dashboard extends Component {
+
   render() {
+    
     return (
       <div>
-        <ReviewOverview />
-        <ReviewDetailInfo />
-        <ReviewRequestTable />
+        <Layout.Section>
+          <Layout.Aside width={200}>
+            <Aside />
+          </Layout.Aside>
+
+          <Layout.Main scrollable>
+            <div className="main-container">
+             {/* <InsideMainRoutes />*/}
+             
+            </div>
+          </Layout.Main>
+        </Layout.Section>
       </div>
     );
   }
